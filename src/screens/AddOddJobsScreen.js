65e6e775d69b80d4ac1a08b3fc324c6d3269c1ee
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-10-30 18:55:25 +0530

    Create Add odd job screen

--- src/screens/AddOddJobsScreen.js
+++ src/screens/AddOddJobsScreen.js
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import commonStyles from "../styles/common";
+import React, { useState } from "react";
+import Stepper from "react-native-stepper-ui";
+import { StyleSheet, Text, View, Alert, ScrollView } from "react-native";
+import {
+  PersonalDetails,
+  JobDetails,
+  SalaryDetails,
+} from "../components/JobCreate";
+
+const content = [<PersonalDetails />, <JobDetails />, <SalaryDetails />];
 
 const AddOddJobsScreen = () => {
+  const [active, setActive] = useState(0);
   return (
-    <View style={commonStyles.container}>
-      <View style={commonStyles.section}>
-        <Text>Add Odd Jobs</Text>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={{ marginVertical: 80, marginHorizontal: 20, marginTop: 10 }}>
+        <Stepper
+          active={active}
+          content={content}
+          onNext={() => setActive((p) => p + 1)}
+          onBack={() => setActive((p) => p - 1)}
+          onFinish={() => Alert.alert("Create odd job sussecfully")}
+        />
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
